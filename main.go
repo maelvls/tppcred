0bd8f28f46c69a4544211727b7afe56ffdc25461
Maël Valais <mael@vls.dev>
2025-01-23 17:34:52 +0100

    add "ls" command

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 const (
@@ -29,20 +30,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	listCmd := flag.NewFlagSet("list", flag.ExitOnError)
+	listCmd := flag.NewFlagSet("ls", flag.ExitOnError)
 	editCmd := flag.NewFlagSet("edit", flag.ExitOnError)
 
 	if len(os.Args) < 2 {
-		fmt.Println("Expected 'list' or 'edit' subcommands")
+		fmt.Println("Expected 'ls' or 'edit' subcommands")
 		os.Exit(1)
 	}
 
 	switch os.Args[1] {
-	case "list":
+	case "ls":
 		listCmd.Parse(os.Args[2:])
-		if err := listConfigs(tppURL, token); err != nil {
+		objs, err := listObjects(tppURL, token)
+		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 			os.Exit(1)
+		}
+		for _, obj := range objs {
+			fmt.Println(obj)
 		}
 	case "edit":
 		editCmd.Parse(os.Args[2:])
@@ -58,10 +63,6 @@ func main() {
 		fmt.Printf("Unknown command: %s\n", os.Args[1])
 		os.Exit(1)
 	}
-}
-
-func listConfigs(tppURL, token string) error {
-	panic("not implemented")
 }
 
 type Credential struct {
@@ -235,3 +236,78 @@ func updateCred(tppURL, token, credPath string, c Credential) error {
 	}
 	return nil
 }
+
+func listObjects(tppURL, token string) ([]string, error) {
+	body, err := json.Marshal(struct {
+		ObjectDN  string `json:"ObjectDN"`
+		Pattern   string `json:"Pattern"`
+		Recursive bool   `json:"Recursive"`
+	}{
+		ObjectDN:  `\VED\Policy`,
+		Recursive: true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("while marshalling request for /vedsdk/Config/Enumerate: %w", err)
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/vedsdk/Config/Enumerate", tppURL), bytes.NewReader(body))
+	if err != nil {
+		return nil, fmt.Errorf("while creating request for /vedsdk/Config/Enumerate: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("User-Agent", userAgent)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("while making request to /vedsdk/Config/Enumerate: %w", err)
+	}
+	defer resp.Body.Close()
+
+	var creds ObjectsResp
+	err = json.NewDecoder(resp.Body).Decode(&creds)
+	if err != nil {
+		return nil, fmt.Errorf("while decoding response from /vedsdk/Config/Enumerate: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		// Dump body.
+		body, _ := io.ReadAll(resp.Body)
+		return nil, fmt.Errorf("list failed: %s: %s", resp.Status, string(body))
+	}
+
+	var credPaths []string
+	for _, cred := range creds.Objects {
+		if !strings.Contains(cred.TypeName, "Generic Credential") {
+			continue
+		}
+		credPaths = append(credPaths, fmt.Sprintf("%s", cred.Dn))
+	}
+	return credPaths, nil
+}
+
+type ObjectsResp struct {
+	Objects []Objects `json:"Objects"`
+	Result  int       `json:"Result"`
+}
+type Objects struct {
+	AbsoluteGUID string `json:"AbsoluteGUID"`
+	Dn           string `json:"DN"`
+	GUID         string `json:"GUID"`
+	ID           int    `json:"Id"`
+	Name         string `json:"Name"`
+	Parent       string `json:"Parent"`
+	Revision     int    `json:"Revision"`
+	TypeName     string `json:"TypeName"`
+}
+
+type TypeName string
+
+const (
+	TypeNameX509ServerCert       TypeName = "X509 Server Certificate"
+	TypeNamePolicy               TypeName = "Policy"
+	TypeNameGoogleCred           TypeName = "Google Credential"
+	TypeNameGenericCred          TypeName = "Generic Credential"
+	TypeNameUsernamePasswordCred TypeName = "Username Password Credential"
+)
